713af5b874d51758b850bc081fcc4587e105b750
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-15 12:55:38 -0600

    added left and right movement

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 let p;
 
 function setup() {
-  createCanvas(400, 400);
-  p = new Player(200,200, 10,10,"#780000");
+  createCanvas(300, 300);
+  p = new Player(150,150, 10,10,"#780000");
 }
 
 function draw() {
   background("#003049");
   p.display();
+  p.update();
 }
