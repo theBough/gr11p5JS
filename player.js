843e6e3e1089795eb23b739a77153105fc1ffa56
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-15 12:55:15 -0600

    left and Right movement

--- player.js
+++ player.js
@@ -1,12 +1,26 @@
 function Player(x,y,w,h,col){
-  this.x = x
-  this.y = y
-  this.w = w 
-  this.h = h 
-  this.col = col
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h; 
+  this.col = col;
   
   this.display = function(){
     fill(this.col)
     rect(this.x, this.y, this.w, this.h)
   }//end display
+  
+  this.update = function(){
+    
+    if(keyIsDown(37)){
+      //left arrow pressed
+      this.x -= 5
+    }//end if
+    
+    if(keyIsDown(39)){
+      //left arrow pressed
+      this.x += 5
+    }//end if
+    
+  }//end update
 }//end player
