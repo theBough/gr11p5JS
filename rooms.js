56e0eb31dc22485cb97ee2bc2dc3435568dfb7e5
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-16 13:16:25 -0600

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,17 +1,29 @@
 let rooms = [];
 
 function fillRooms(){
-  rooms = [roomZero, roomOne, roomTwo]
+  rooms = [
+    [roomZero, roomOne, roomTwo],
+    [roomThree, roomFour, roomFive]
+    ]
 }//end fillRooms
-
 let roomZero = function(){
   backColour = "pink"
 }//end RoomZero
-
 let roomOne = function(){
   backColour = "red"
 }//end RoomOne
-
 let roomTwo = function(){
   backColour = "purple"
 }//end RoomTwo
+let roomThree = function(){
+  backColour = "brown"
+}//end RoomZero
+
+let roomFour = function(){
+  backColour = "black"
+}//end RoomOne
+
+let roomFive = function(){
+  backColour = "grey"
+}//end RoomTwo
+
