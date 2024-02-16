d0509672a1eea7d988f45b38a824cd272cf5ff86
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-16 12:55:49 -0600

    added Rooms Left and Right

--- sketch.js
+++ sketch.js
@@ -1,21 +1,43 @@
 let p;
+let backColour = "#003049"
+let activeX, activeY;
+
 
 function setup() {
   createCanvas(300, 300);
   p = new Player(150,150, 10,10,"#780000");
+  activeX = 0;
+  fillRooms();
 }
 
 function draw() {
-  background("#003049");
+  background(backColour);
   p.display();
   p.update();
   screenChange();
+  rooms[activeX].call();
 }
 
 function screenChange(){
   if(p.x > width){
     //The play has crossed the right edge
+   activeX += 1
     p.x=0
+  }//end if
+   if(p.x < 0){
+    //The play has crossed the right edge
+   activeX -= 1;
+    p.x=width
+  }//end if
+  if(p.y > height){
+    //The play has crossed the right edge
+    backColour = "yellow"
+    p.y=0
+  }//end if
+   if(p.y < 0){
+    //The play has crossed the right edge
+    backColour = "#003049"
+    p.y=height
   }//end if
   
 }//end screenchange
