730ce63845fc13c7f0d552bdb94d48c19aa2ce04
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-16 12:44:38 -0600

    Create rooms.js

---
+++ rooms.js
@@ -0,0 +1,17 @@
+let rooms = [];
+
+function fillRooms(){
+  rooms = [roomZero, roomOne, roomTwo]
+}//end fillRooms
+
+let roomZero = function(){
+  backColour = "pink"
+}//end RoomZero
+
+let roomOne = function(){
+  backColour = "red"
+}//end RoomOne
+
+let roomTwo = function(){
+  backColour = "purple"
+}//end RoomTwo
