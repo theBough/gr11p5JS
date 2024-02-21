8291079e78314f9c14c0e4ce1a5dff3ccc39f74d
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-21 13:22:22 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,24 @@
 let p;
 let backColour = "#003049"
 let activeX, activeY;
+let w =[]
+
 function setup() {
   createCanvas(300, 300);
   p = new Player(150,150, 10,10,"#780000");
   activeX = 0;
   activeY = 0;
   fillRooms();
+ 
 }
 function draw() {
   background(backColour);
   p.display();
   p.update();
+  for(i=0 ; i < w.length ; i++){
+    w[i].display() 
+  }//end loop
+  w.display(); 
   screenChange();
   rooms[activeY][activeX].call();
 }
