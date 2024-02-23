4987db57d06fdf0270ef073a8d5e0e5ec3bac27f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-23 12:59:40 -0600

    Create key.js

---
+++ key.js
@@ -0,0 +1,33 @@
+function Key(x,y,w,h){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.r = rooms[0][0];
+  this.have = false;
+  this.img("key.jpg")
+  
+  this.display = function(){
+    if(rooms[ActiveY][activeX] === this.r){
+      this.img.resize(this.w, this.h);
+      image(this.img, this.x, this.y)
+    }
+  }//end display
+  this.keyCollision = function(){
+    if(
+      p.y <= this.y+ this.h &&
+      p.y +p.h >= this.y &&
+      p.x + p.w  >= this.x &&
+      p.x <= this.x +this.w &&
+      this.r === rooms[activeY][activeX]
+    ){
+      this.have = true;
+    }
+  }//end collision
+  this.update = function(){
+    if(this.have){
+      this.x = p.x;
+      this.y = p.y;
+    }
+  }//end update
+}//end Key
