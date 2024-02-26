05d42de870e8f5fbeb1c489bf1d605f1823267d2
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-26 13:18:12 -0600

    Update gate.js

--- gate.js
+++ gate.js
@@ -8,6 +8,9 @@ function Gate(x,y,w,h,col,room){
   this.locked = true;
   
   this.display = function(){
-    
-  }
-}
+    if(rooms[activeY][activeX] === this.room && this.locked){
+      fill(this.col)
+      rect(this.x, this.y, this.w, this.h)
+    } //end if
+  }//end display
+}//end Gate
