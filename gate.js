db52cfd20a3a4671c60b8281a92e7df90e1e5bd0
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-26 13:15:03 -0600

    Create gate.js

---
+++ gate.js
@@ -0,0 +1,13 @@
+function Gate(x,y,w,h,col,room){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.col = col;
+  this.room = room;
+  this.locked = true;
+  
+  this.display = function(){
+    
+  }
+}
