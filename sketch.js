9da724d1c5e5314eb1fdb801f8ad618b1e213407
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-27 12:42:16 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
-let p,k,g;
-let backColour = "#003049"
+let p, k, g;
+let backColour = "#003049";
 let activeX, activeY;
-let w =[]
+let w = [];
 
 function setup() {
   createCanvas(300, 300);
   fillRooms();
-  p = new Player(150,150, 10,10,"#FFFFFF");
-  k = new Key(200,200,10,10);
-  g = new Gate(175,0,10,height,"#FFFFFF",rooms[0][0]);
+  p = new Player(150, 150, 10, 10, "#FFFFFF");
+  k = new Key(100, 200, 10, 10);
+  g = new Gate(175, 0, 10, height, "#FFFFFF", rooms[0][0]);
   activeX = 0;
   activeY = 0;
 }
@@ -21,46 +21,43 @@ function draw() {
   screenChange();
   rooms[activeY][activeX].call();
 }
-function gateStuff(){
+function gateStuff() {
   g.display();
   g.collision();
+  g.keyCollision();
 }
-function wallStuff(){
-   for(i=0 ; i < w.length ; i++){
-    w[i].display() 
-  }//end loop
+function wallStuff() {
+  for (i = 0; i < w.length; i++) {
+    w[i].display();
+  } //end loop
   checkForCollision();
 }
-function keyStuff(){
-   k.display();
+function keyStuff() {
+  k.display();
   k.keyCollision();
-  k.update()  
-}                   
-function playerStuff(){
-   p.display();
+  k.update();
+}
+function playerStuff() {
+  p.display();
   p.update();
 }
-function screenChange(){
-  if(p.x > width){
+function screenChange() {
+  if (p.x > width) {
     //The play has crossed the right edge
-   activeX += 1
-    p.x=0
-  }//end if
-   if(p.x < 0){
+    activeX += 1;
+    p.x = 0;
+  } //end if
+  if (p.x < 0) {
     //The play has crossed the right edge
-   activeX -= 1;
-    p.x=width
-  }//end if
-  if(p.y > height){
-    activeY += 1
-    p.y=0
-  }//end if
-   if(p.y < 0){
-   activeY -= 1
-    p.y=height
-  }//end if
-  
-}//end screenchange
-
-
-
+    activeX -= 1;
+    p.x = width;
+  } //end if
+  if (p.y > height) {
+    activeY += 1;
+    p.y = 0;
+  } //end if
+  if (p.y < 0) {
+    activeY -= 1;
+    p.y = height;
+  } //end if
+} //end screenchange
