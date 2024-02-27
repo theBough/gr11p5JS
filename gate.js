e88baf185e9845724ed4070aa66cc6145aa86621
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-02-27 12:38:45 -0600

    key opens gate.

--- gate.js
+++ gate.js
@@ -1,4 +1,4 @@
-function Gate(x,y,w,h,col,room){
+function Gate(x, y, w, h, col, room) {
   this.x = x;
   this.y = y;
   this.w = w;
@@ -6,32 +6,90 @@ function Gate(x,y,w,h,col,room){
   this.col = col;
   this.room = room;
   this.locked = true;
-  
-  this.display = function(){
-    if(rooms[activeY][activeX] === this.room && this.locked){
-      fill(this.col)
-      rect(this.x, this.y, this.w, this.h)
+
+  this.display = function () {
+    if (rooms[activeY][activeX] === this.room && this.locked) {
+      fill(this.col);
+      rect(this.x, this.y, this.w, this.h);
     } //end if
-  }//end display
-  
-  this.collision = function(){
-    if(rooms[activeY][activeX] == this.room){
-       //check if we hit the left of any wall
-    if (p.y <= this.y + this.h && p.y + p.h >= this.y && p.x <= this.x + this.w && p.x >= this.x) {
-      p.x += 5
+  }; //end display
+
+  this.collision = function () {
+    if (rooms[activeY][activeX] == this.room) {
+      //check if we hit the left of any wall
+      if (
+        k.y <= this.y + this.h &&
+        k.y + k.h >= this.y &&
+        k.x <= this.x + this.w &&
+        k.x >= this.x
+      ) {
+        k.x += 5;
+      }
+      //check if we hit the right of any wall
+      if (
+        k.y <= this.y + this.h &&
+        k.y + k.h >= this.y &&
+        k.x + k.w >= this.x &&
+        k.x <= this.x + this.w
+      ) {
+        k.x -= 5;
+      }
+      if (
+        k.x <= this.x + this.w &&
+        k.x + k.w >= this.x &&
+        k.y <= this.y + this.h &&
+        k.y >= this.y
+      ) {
+        k.y += 5;
+      }
+      //check if we hit the top of any wall
+      if (
+        k.x <= this.x + this.w &&
+        k.x + k.w >= this.x &&
+        k.y + k.h >= this.y &&
+        k.y <= this.y + this.h
+      ) {
+        k.y -= 5;
+      }
     }
-    //check if we hit the right of any wall
-    if (p.y <= this.y + this.h && p.y + p.h >= this.y && p.x + p.w >= this.x && p.x <= this.x + this.w) {
-      p.x -= 5
+  }; //end collision
+   this.keyCollision = function () {
+    if (rooms[activeY][activeX] == this.room) {
+      //check if we hit the left of any wall
+      if (
+        k.y <= this.y + this.h &&
+        k.y + k.h >= this.y &&
+        k.x <= this.x + this.w &&
+        k.x >= this.x
+      ) {
+       this.locked =false;
+      }
+      //check if we hit the right of any wall
+      if (
+        k.y <= this.y + this.h &&
+        k.y + k.h >= this.y &&
+        k.x + k.w >= this.x &&
+        k.x <= this.x + this.w
+      ) {
+        this.locked =false;
+      }
+      if (
+        k.x <= this.x + this.w &&
+        k.x + k.w >= this.x &&
+        k.y <= this.y + this.h &&
+        k.y >= this.y
+      ) {
+        this.locked =false;
+      }
+      //check if we hit the top of any wall
+      if (
+        k.x <= this.x + this.w &&
+        k.x + k.w >= this.x &&
+        k.y + k.h >= this.y &&
+        k.y <= this.y + this.h
+      ) {
+        this.locked =false;
+      }
     }
-    if (p.x <= this.x + this.w && p.x + p.w >= this.x && p.y <= this.y + this.h && p.y >= this.y) {
-      p.y += 5;
-    }
-    //check if we hit the top of any wall
-    if (p.x <= this.x + this.w && p.x + p.w >= this.x && p.y + p.h >= this.y && p.y <= this.y + this.h) {
-      p.y -= 5;
-    }
-    }
-   
-  }//end collision
-}//end Gate
+  }; //end collision
+} //end Gate
