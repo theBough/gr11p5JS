5362ad2ec401f77de87da379654e6a781ced4348
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-03-01 13:13:04 -0600

    Update key.js

--- key.js
+++ key.js
@@ -1,33 +1,36 @@
-function Key(x,y,w,h){
+function Key(x, y, w, h) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.r = rooms[0][0];
   this.have = false;
-  this.img = loadImage("niners.png")
-  
-  this.display = function(){
-    if(rooms[activeY][activeX] === this.r  || this.have){
-      this.img.resize(this.w, this.h);
-      image(this.img, this.x, this.y)
+  this.img = loadImage("keyAdventure.jpg");
+  this.show = true;
+
+  this.display = function () {
+    if (this.show) {
+      if (rooms[activeY][activeX] === this.r || this.have) {
+        this.img.resize(this.w, this.h);
+        image(this.img, this.x, this.y);
+      }
     }
-  }//end display
-  this.keyCollision = function(){
-    if(
-      p.y <= this.y+ this.h &&
-      p.y +p.h >= this.y &&
-      p.x + p.w  >= this.x &&
-      p.x <= this.x +this.w &&
+  }; //end display
+  this.keyCollision = function () {
+    if (
+      p.y <= this.y + this.h &&
+      p.y + p.h >= this.y &&
+      p.x + p.w >= this.x &&
+      p.x <= this.x + this.w &&
       this.r === rooms[activeY][activeX]
-    ){
+    ) {
       this.have = true;
     }
-  }//end collision
-  this.update = function(){
-    if(this.have){
+  }; //end collision
+  this.update = function () {
+    if (this.have) {
       this.x = p.x;
       this.y = p.y;
     }
-  }//end update
-}//end Key
+  }; //end update
+} //end Key
